f3a6d16a8ead2ffc325c77494c0c21c3072111ea
webbuf <zwebb@ufl.edu>
2022-11-27 09:39:27 -0500

    parts 2 and 3

---
+++ distanceAlongParametric.m
@@ -0,0 +1,39 @@
+function tStar = distanceAlongParametric(x, y, a, b, s)
+%Find the point t*(s) that is s of the way along a curve
+%x and y and the x and y functions that define a parametric curve
+%a and b are the start and end points of the range to search. While our
+%full path is always from 0 to 1, changing a and b allows us to reduce our
+%search time by not always checking the full array. This is used in
+%question 3. 
+%s is the distance along the curve we want to find
+
+syms t
+xPrime = diff(x);
+yPrime = diff(y);
+%get the derivatives of x and y, which we need for computing arc length
+arcLengthPortion = matlabFunction(sqrt(xPrime^2 + yPrime^2));
+%get the function we put in the integral. matlab function allows it to be
+%used with numerical integration, as matlab as trouble integrating this
+%symbolically
+totalArcLength = integral(arcLengthPortion, 0, 1);
+%get the arc length from 0 to 1
+
+while(true)
+    m = (a + b)/2;
+    %bisection method. get our midpoint
+    lengthToM = integral(arcLengthPortion, 0, m);
+    currentS = lengthToM/totalArcLength;
+    %get length from 0 to the midpoint, and then find the ratio of that to
+    %the total length
+    if (abs(s - currentS)) < 0.5 * 10^-3
+        break
+    end
+    %if we're within our error, exit the loop
+    if(currentS < s)
+        a = m;
+    else
+        b = m;
+    end
+    %otherwise, readjust our bounds and repeat
+end
+tStar = m;
---
+++ equipartition.m
@@ -0,0 +1,56 @@
+function partitionBounds = equipartition(x, y, n)
+%x and y are the parametric functions
+%n is the target number of partitions to split into
+midpoint = floor(n/2);
+firstQuartile = floor(n/4);
+thirdQuartile = floor(3 * n / 4);
+%we get some early points to serve as bounds
+%now all bisections will only have to go over one fourth of the graph
+
+h = 1/n;
+%define our step size
+
+results = 1:n+1;
+results(1) = 0;
+results(n + 1) = 1;
+%set up vector with partition bounds, set first to 0 and last to 1
+
+results(midpoint + 1) = distanceAlongParametric(x, y, 0, 1, midpoint * h);
+results(firstQuartile + 1) = distanceAlongParametric(x, y, 0, results(midpoint + 1), firstQuartile * h);
+results(thirdQuartile + 1) = distanceAlongParametric(x, y, results(midpoint + 1), 1, thirdQuartile * h);
+%find values for partition bounds
+%our first bound is 0, so all caculated bounds are indexed one more than
+%their actual "position"
+
+for i = 1:firstQuartile - 1
+    results(i + 1) = distanceAlongParametric(x, y, 0, results(firstQuartile + 1), i * h);
+end
+
+for i = firstQuartile + 1:midpoint - 1
+    results(i + 1) = distanceAlongParametric(x, y, results(firstQuartile + 1), results(midpoint + 1), i * h);
+end
+
+for i = midpoint + 1:thirdQuartile - 1
+    results(i + 1) = distanceAlongParametric(x, y, results(midpoint + 1), results(thirdQuartile + 1), i * h);
+end
+
+for i = thirdQuartile + 1:n - 1
+    results(i + 1) = distanceAlongParametric(x, y, results(thirdQuartile + 1), 1, i * h);
+end
+%finding the rest of the values is done over 4 loops, each account for
+%1/4th the total length of the arc. this should speed up calculation, as
+%each bound is now already 1/4th the total
+
+
+hold on
+for i=1:n
+    t = linspace(results(i), results(i + 1));
+    x = 0.5 + 0.3*t + 3.9*t.^2 - 4.7*t.^3;
+    y = 1.5 + 0.3*t + 0.9*t.^2 - 2.7*t.^3;
+    plot(x, y);
+end
+hold off
+%plot based off our bounds
+
+partitionBounds = results;
+end
