59ab6fad63a806ceb1e1ddf754b8963f39658d81
Ty Beller <73607363+tybeller@users.noreply.github.com>
2022-12-01 03:18:20 -0500

    Fixed bounds of window
    Bounds now account for curves where the max values are not at the endpoints

--- traverseProgressCurve.m
+++ traverseProgressCurve.m
@@ -3,7 +3,7 @@ function [] = traverseProgressCurve(Px, Py, C)
 %C is the arbitrary progress curve which defines the speed at which the
 %function is traversed
 
-set(gca,'XLim', [floor(Px(0)) ceil(Px(1))], 'YLim', [floor(Py(0)) ceil(Py(1))], 'Drawmode','fast','Visible','on');
+set(gca,'XLim', [floor(min(Px(0:0.01:1))) ceil(max(Px(0:0.01:1)))], 'YLim', [floor(min(Py(0:0.01:1))) ceil(max(Py(0:0.01:1)))], 'Drawmode','fast','Visible','on');
 %create the window to draw the animation
 %bounds set by the bounds of the function, rounded up
 cla
