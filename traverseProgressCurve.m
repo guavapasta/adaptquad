f3c186fbe61aa7c05285b7bc05cbf3caef201c9d
Ty Beller <73607363+tybeller@users.noreply.github.com>
2022-12-02 17:20:24 -0500

    Added plot for curve

--- traverseProgressCurve.m
+++ traverseProgressCurve.m
@@ -3,12 +3,15 @@ function [] = traverseProgressCurve(Px, Py, C)
 %C is the arbitrary progress curve which defines the speed at which the
 %function is traversed
 
+
 set(gca,'XLim', [floor(min(Px(0:0.01:1))) ceil(max(Px(0:0.01:1)))], 'YLim', [floor(min(Py(0:0.01:1))) ceil(max(Py(0:0.01:1)))], 'Drawmode','fast','Visible','on');
 %create the window to draw the animation
 %bounds set by the bounds of the function, rounded up
 cla
-axis equal
-pointer = animatedline('color','r','Marker','o','MarkerSize',2,'MarkerFaceColor','r');
+axis off
+i = 0:0.01:1;
+plot(Px(i),Py(i))
+pointer = animatedline('color','r','Marker','o','MarkerSize',5,'MarkerFaceColor','r');
 
 for s = 0:0.01:1
     t = distanceAlongParametric(Px, Py, 0, 1, C(s));
